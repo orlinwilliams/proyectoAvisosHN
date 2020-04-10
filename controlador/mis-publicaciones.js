4e4589ec3ece55de42e7cededee725826542bc25
LITO <orlinwilliams0077@gmail.com>
2020-04-09 22:41:35 -0600

    mis publicaciones

--- controlador/mis-publicaciones.js
+++ controlador/mis-publicaciones.js
@@ -1,6 +1,67 @@
 $(document).ready(function () {
     municipios();																			//Llama la funcion municipios
     misPublicaciones();
+    Dropzone.autoDiscover = false;
+    myDropzone = new Dropzone('div#subirFotos', {
+        addRemoveLinks: true,
+        autoProcessQueue: false,
+        uploadMultiple: true,
+        parallelUploads: 100,
+        maxFiles: 4,
+        paramName: 'file',
+        clickable: true,
+        url: '../clases/vistas-index.php?accion=2',
+        init: function () {
+    
+            var myDropzone = this;
+            // Update selector to match your button
+            $("#publicarArticulo").submit(function (e) {
+                event.stopPropagation();
+                e.preventDefault();
+                if ( $("#publicarArticulo").valid() ) {
+                    myDropzone.processQueue();
+                }
+                return false;
+            });
+    
+            this.on('sending', function (file, xhr, formData) {
+                // Append all form inputs to the formData Dropzone will POST
+                var data = $("#publicarArticulo").serializeArray();
+                $.each(data, function (key, el) {
+                    formData.append(el.name, el.value);
+                });
+                //console.log(formData);
+    
+            });
+        },
+        error: function (file, response){
+            if ($.type(response) === "string")
+                var message = response; //dropzone sends it's own error messages in string
+            else
+                var message = response.message;
+            file.previewElement.classList.add("dz-error");
+            _ref = file.previewElement.querySelectorAll("[data-dz-errormessage]");
+            _results = [];
+            for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+                node = _ref[_i];
+                _results.push(node.textContent = message);
+            }
+            return _results;
+        },
+        successmultiple: function (file, response) {
+            console.log(file, response);
+            //$modal.modal("show");
+        },
+        completemultiple: function (file, response) {
+            console.log(file, response, "completemultiple");
+            //$modal.modal("show");
+        },
+        reset: function () {
+            console.log("resetFiles");
+            //this.removeAllFiles(true);
+        }
+      
+    });
 
 });
 municipios = function () {														//Inicio funcion para llenar los municipios
@@ -24,7 +85,7 @@ misPublicaciones = function () { //VISTA DE MIS PUBLIACIONES
                 tarjetas += "<div class='col-sm-6 col-md-6 col-lg-3 cards'>"
                     + "<div class='carde'>"
                     + "<div class='card__image-holder'>"
-                    + "<img class='card__image' src='../images/5e82b609678c10101241D3' alt='Miniatura del anuncio' max-width='100%;' height='auto;'/>"
+                    + "<img class='card__image' src='"+item.fotos[0]+"' alt='Miniatura del anuncio' max-width='100%;' height='auto;'/>"
                     + "</div>"
                     + "<div class='card-title'>"
                     + "<a  href='#' class='toggle-info btn'>"
